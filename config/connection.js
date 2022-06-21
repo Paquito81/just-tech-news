cb36cdd4d01e7788282071a00a0c6ba4f640bcee
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-20 23:48:13 -0700

    add

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+//import the sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports =sequelize;
