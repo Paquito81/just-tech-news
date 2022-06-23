4e551db6b809a9e4e83b803cc3450c9480eed08c
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-23 15:31:54 -0700

    add

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { route } = require('./api');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+route.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
