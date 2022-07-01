0e23edc3e695fdbcc1ee6c81e37f03aa38a562f3
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-30 23:42:37 -0700

    connect to jawsdb

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-require('dotenv').config();
-const mysql = require('mysql2');
-const { dirname } = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+ 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
